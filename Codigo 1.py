e12db4ebd96dd4741c4d41f38edbf52850135ac0
DanB-90 <betancur.t@gmail.com>
2025-09-30 17:55:10 -0500

    Update Codigo 1.py

--- Codigo 1.py
+++ Codigo 1.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
-import plotly.express as px
 from plotly.subplots import make_subplots
 import time
 
