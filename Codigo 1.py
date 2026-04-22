28080b62ea6bdbf5e065b030180cebf5683d8b6b
Isabela <isabelaaqq@gmail.com>
2026-04-22 17:27:21 -0500

    Update Codigo 1.py

--- Codigo 1.py
+++ Codigo 1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from statistics import LinearRegression
+from sklearn.linear_model import LinearRegression
 
 st.set_page_config(
     page_title="Electrolizador PEM",
